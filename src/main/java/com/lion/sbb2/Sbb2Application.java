cd32851c49ec32299d70c9ddfdd6cff40bf491e7
Y39 <y39scode@gmail.com>
2023-03-27 13:38:03 +0900

    setting

---
+++ src/main/java/com/lion/sbb2/Sbb2Application.java
@@ -0,0 +1,13 @@
+package com.lion.sbb2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Spring`BootApplication
+public class Sbb2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sbb2Application.class, args);
+	}
+
+}
