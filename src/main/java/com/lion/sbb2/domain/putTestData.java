920f5c804593966d0acd9dfffd73885f12291045
Y39 <y39scode@gmail.com>
2023-03-28 15:42:29 +0900

    feat: main nav

--- src/main/java/com/lion/sbb2/domain/putTestData.java
+++ src/main/java/com/lion/sbb2/domain/putTestData.java
@@ -13,8 +13,13 @@ public class putTestData {
     @Bean
     public CommandLineRunner insert(QuestionService service){
         return args -> {
-          service.save("sbb는 무엇을 하는 곳인가요?", "항상 궁금했습니다.");
-          service.save("spring boot에서만 jpa를 쓰나요?","jpa 쓰는게 어렵네요.");
+//          service.save("sbb는 무엇을 하는 곳인가요?", "항상 궁금했습니다.");
+//          service.save("spring boot에서만 jpa를 쓰나요?","jpa 쓰는게 어렵네요.");
+            for (int i = 1; i <= 300; i++) {
+                String subject = String.format("테스트 데이터입니다:[%03d]", i);
+                String content = "내용무";
+                service.save(subject, content);
+            }
         };
     }
 }
